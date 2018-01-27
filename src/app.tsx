832c25fd3e5b9b90570acc74b1df54644bb9ddca
Ryan Will <ryanwilldev@gmail.com>
2018-01-27 17:43:44 -0600

    renders zipcodeinput

--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,14 @@ import { h } from 'preact';
 
 import './style.scss';
 import { Header } from './components';
+import { ZipCodeInput } from './components/zip-code-input/zipCodeInput';
 
 const App = () => (
   <div style={{ width: '100vw', height: '100vh' }}>
     <Header />
+    <main style={{ padding: '0 2%' }}>
+      <ZipCodeInput />
+    </main>
   </div>
 );
 
