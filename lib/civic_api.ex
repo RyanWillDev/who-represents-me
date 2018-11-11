284acd7734d78ac06fb8e5c769e8ea07d11cb6ee
Ryan Will <ryanwilldev@gmail.com>
2018-11-11 11:50:02 -0600

    adds function to get representatives for a given address

--- lib/civic_api.ex
+++ lib/civic_api.ex
@@ -1,7 +1,28 @@
 defmodule CivicApi do
-  @key Application.get_env(:civic_api, :api_key)
+  use Tesla
+  plug Tesla.Middleware.BaseUrl, "https://www.googleapis.com/civicinfo/v2"
 
-  def key do
-    @key
+  plug Tesla.Middleware.Query,
+    key: Application.get_env(:who_represents_me, :civic_api, :api_key) |> Keyword.get(:api_key)
+
+  plug Tesla.Middleware.JSON
+
+  def representatives(address) do
+    get("/representatives",
+      query: [
+        address: address,
+        levels: [
+          "administrativeArea1",
+          "administrativeArea2",
+          "country",
+          "international",
+          "locality",
+          "regional",
+          "special",
+          "subLocality1",
+          "subLocality2"
+        ]
+      ]
+    )
   end
 end
