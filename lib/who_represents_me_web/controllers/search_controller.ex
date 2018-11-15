3a7d27f57b853fc2436f170222363622f00c3a5e
Ryan Will <ryanwilldev@gmail.com>
2018-11-15 09:14:52 -0600

    renders basic data for all returned reps

--- lib/who_represents_me_web/controllers/search_controller.ex
+++ lib/who_represents_me_web/controllers/search_controller.ex
@@ -2,6 +2,7 @@ defmodule WRMWeb.SearchController do
   use WRMWeb, :controller
 
   def search(conn, %{"address" => address}) do
-    render(conn, "results.html", address: address)
+    reps = CivicApi.representatives(address)
+    render(conn, "results.html", address: address, reps: reps)
   end
 end
