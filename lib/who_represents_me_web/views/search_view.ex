38ac2e7a79eda421bfa9d838090d4a89512619f5
Ryan Will <ryanwilldev@gmail.com>
2018-11-18 13:19:20 -0600

    adds helper function for generating link color class"

--- lib/who_represents_me_web/views/search_view.ex
+++ lib/who_represents_me_web/views/search_view.ex
@@ -1,3 +1,15 @@
 defmodule WRMWeb.SearchView do
   use WRMWeb, :view
+
+  def link_class(%{"party" => "Republican"}) do
+    "text-republican-red"
+  end
+
+  def link_class(%{"party" => "Democratic"}) do
+    "text-democrat-blue"
+  end
+
+  def link_class(%{"party" => _}) do
+    "text-purple-dark"
+  end
 end
