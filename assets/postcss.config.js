7c898551b3a04b3cca6fa4a6abb0d2d69d2c3ea1
Ryan Will <ryanwilldev@gmail.com>
2018-12-17 11:32:49 -0600

    adds purgecss to remove unused tailwind classes reduces the css from 400kb+ to 14kb : )

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,7 +1,23 @@
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
+
 module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss')('./tailwind.js'),
+    require('@fullhuman/postcss-purgecss')({
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+          extensions: ['html.eex'],
+        },
+      ],
+      content: ['../lib/**/**/*.html.eex'], // files to extract the selectors from
+      css: ['css/app.css'], // css
+    }),
     require('autoprefixer'),
   ],
 };
