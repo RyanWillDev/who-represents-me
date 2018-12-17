c37bf10fd399e782f1d69483d45146c26d564ba2
Ryan Will <ryanwilldev@gmail.com>
2018-12-17 17:30:47 -0600

    adds adapter to repo config

--- config/heroku.exs
+++ config/heroku.exs
@@ -21,8 +21,10 @@ config :logger, level: :info
 
 # Configure your database
 config :who_represents_me, WRM.Repo,
+  adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
