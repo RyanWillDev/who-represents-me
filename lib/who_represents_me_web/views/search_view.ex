21d9036ea577968f6ff1087b66ff135acb419d34
Ryan Will <ryanwilldev@gmail.com>
2018-12-20 11:56:12 -0600

    removes unused function and link colors, adds details button to congress members

--- lib/who_represents_me_web/views/search_view.ex
+++ lib/who_represents_me_web/views/search_view.ex
@@ -1,18 +1,6 @@
 defmodule WRMWeb.SearchView do
   use WRMWeb, :view
 
-  def link_class(%{"party" => "Republican"}) do
-    "text-republican-red"
-  end
-
-  def link_class(%{"party" => "Democratic"}) do
-    "text-democrat-blue"
-  end
-
-  def link_class(%{"party" => _}) do
-    "text-purple-dark"
-  end
-
   def social_link(%{"id" => id, "type" => "Facebook"}) do
     "https://facebook.com/" <> id
   end
