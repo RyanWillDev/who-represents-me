ce9127c55e05bc2f9a464b989d8b7ac1d1004f09
Ryan Will <ryanwilldev@gmail.com>
2018-12-24 18:34:39 -0600

    basic web component setup to render dummy data until promise is resolved

--- assets/js/components/votingRecord.js
+++ assets/js/components/votingRecord.js
@@ -1,13 +1,34 @@
 import { html, LitElement } from '@polymer/lit-element';
+import { until } from 'lit-html/directives/until';
+import http from '../http';
 
 class VotingRecord extends LitElement {
   constructor() {
     super();
   }
 
+  static get properties() {
+    return {
+      propublicaId: String,
+    };
+  }
+
+  getTabContent(data) {
+    return html`
+      <h1>${data.first_name}</h1>
+    `;
+  }
+
+  connectedCallback() {
+    this.tabContent = http
+      .get(`congress_members/${this.propublicaId}/details`)
+      .then(res => res.data)
+      .then(this.getTabContent);
+  }
+
   render() {
     return html`
-      <div><h1>Test</h1></div>
+      <div>${until(this.tabContent, `Loading`)}</div>
     `;
   }
 }
